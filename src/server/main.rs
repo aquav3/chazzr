87d0b1466112895f24c375c5b8dd1c346578423e
aquav3 <kyrbynyt@gmail.com>
2023-07-08 20:47:35 +0300

    Improving error handling in the server binding code  - Moved binding to ip to a separate function

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,15 +1,22 @@
 use std::net::TcpListener;
 
-fn main() {
-    let mut listener = TcpListener::bind("127.0.0.1:80");
-
-    if let Ok(ls) = listener {
-        listener = Ok(ls);
+fn bind_to_ip(ip: &str) -> Option<TcpListener> {
+    if let Ok(ls) = TcpListener::bind(ip) {
         println!("Bound to ip address succesfully!");
+
+        return Some(ls)
+
     }
-    else {
-        println!("Failed to bind to ip address!");
-        return
-    }
+    println!("Failed to bind to ip address!");
+    return None;
+}
      
+
+fn main() {
+    if let Some(listener) = bind_to_ip("127.0.0.1:8080") {
+        
+    }   
+    else {
+        println!("Exiting the program due to the error!");
+    }
 }
