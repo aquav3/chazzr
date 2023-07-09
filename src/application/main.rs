3fc0c9ed703ba6876ee38bcd0eaf86507ff9c350
aquav3 <kyrbynyt@gmail.com>
2023-07-09 14:46:12 +0300

    Application connection handling

--- src/application/main.rs
+++ src/application/main.rs
@@ -1,3 +1,13 @@
+use std::net::*;
+
+fn connect_to_ip(ip: &str) -> Option<TcpStream> {
+    match TcpStream::connect(ip) {
+        Ok(stream) => return Some(stream),
+        Err(_) => return None,
+    }
+}
+
 fn main() {
-    println!("Hello, world!"); 
+    let mut stream = connect_to_ip("127.0.0.1:8080");
+
 }
