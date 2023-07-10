07bc553a3af3962c36962e7247ca17b995fc80fd
aquav3 <kyrbynyt@gmail.com>
2023-07-10 18:35:30 +0300

    Fixed the flooding of newlines everytime a client closes a connection

--- src/application/main.rs
+++ src/application/main.rs
@@ -1,5 +1,5 @@
 use std::net::*;
-use std::io::Write;
+use std::io::{Write, Read};
 use serde::*;
 
 #[derive(Default, Serialize, Deserialize)]
@@ -17,22 +17,24 @@ fn connect_to_ip(ip: &str) -> Option<TcpStream> {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut user_data = UserData::default();    
+    let mut user_data = UserData::default();
+
     println!("Enter your username.");
     std::io::stdin().read_line(&mut user_data.name).expect("Failed to read input!");
     user_data.name = user_data.name.trim().to_string();
+
     'running: loop {
         user_data.message = "".to_string();
         std::io::stdin().read_line(&mut user_data.message).expect("Failed to read input!");
         user_data.message = user_data.message.trim().to_string();
 
         if user_data.message == "exit" {
-            stream.shutdown(Shutdown::Both).unwrap();
             break 'running;
         }
         let data = serde_json::to_string(&user_data).unwrap();
         println!("{}", data);
         stream.write(data.as_bytes()).unwrap();
+            
     }
 }
 
